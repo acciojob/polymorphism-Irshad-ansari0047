a25514006ac57bf991b22d8b4e4ca76f1aaae960
Irshad-ansari0047 <irshadansari0047@gmail.com>
2024-11-11 00:20:49 +0530

    udated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -21,9 +21,9 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
 
-       System.out.println(p.product(10,20));
-        System.out.println(p.product(2,4,6));
-        System.out.println(p.product(10.2,7.5));
+        System.out.println( p.product(10,20));
+        System.out.println( p.product(2,4,6));
+        System.out.println( p.product(10.2,7.5));
         
 
     }
